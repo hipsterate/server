9fd6004f3f436a53a5918798460afd8dc6fd427c
qodot <waitingforqodot@gmail.com>
2018-12-16 18:27:24 +0900

    유저 이름에 null 허용

--- app/user/model.py
+++ app/user/model.py
@@ -5,8 +5,9 @@ from app.common.db import db, DateTimeUTC
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(80), unique=True)
     lastfm_name = db.Column(db.String(80), unique=True)
     create_time = db.Column(
         DateTimeUTC, nullable=False, default=datetime.now())
