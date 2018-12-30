a747789c3322b67fd4e5b854df9c9d431d141678
qodot <waitingforqodot@gmail.com>
2018-12-30 16:27:44 +0900

    소셜 회원가입 서비스 추가

--- app/user/command.py
+++ app/user/command.py
@@ -1,14 +1,26 @@
 from flask_login import login_user
 
 from app.common.command import Command
+from app.common.external import Facebook
 
-from .model import User
+from .model import User, UserSocial
 
 
 class UserCommand(Command):
-    def signup(self, email, password):
-        new_user = User(email=email, password=password)
+    def signup(self, name, email, password):
+        new_user = User(name=name, email=email, password=password)
         self.insert(new_user)
+
+    def social_signup(self, provider, id_, access_token):
+        if provider == 'facebook':
+            fetched = Facebook.get_user(id_, access_token)
+        else:
+            raise ValueError
+        new_user = User(email=fetched['email'], name=fetched['name'])
+        self.insert(new_user)
+        new_social = UserSocial(
+                user=new_user, social_provider=provider, social_id=id_)
+        self.insert(new_social)
 
     def signin(self, user):
         login_user(user)
